9153a413dce97c7f3d168e85432bb29ff56bfc5d
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2025-04-18 21:44:12 -0500

    feat: add custom fonts

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr08_20251/lab1/ui/theme/Type.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr08_20251/lab1/ui/theme/Type.kt
@@ -2,11 +2,35 @@ package co.edu.udea.compumovil.gr08_20251.lab1.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import co.edu.udea.compumovil.gr08_20251.lab1.R
 
-// Set of Material typography styles to start with
+val balinookRegular = FontFamily(
+    Font(R.font.balinook_regular)
+)
+
+val balinookBold = FontFamily(
+    Font(R.font.balinook_bold)
+)
+
+val CustomTypography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = balinookRegular,
+        fontSize = 16.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = balinookBold,
+        fontSize = 25.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = balinookRegular,
+        fontSize = 11.sp
+    )
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -15,20 +39,4 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
