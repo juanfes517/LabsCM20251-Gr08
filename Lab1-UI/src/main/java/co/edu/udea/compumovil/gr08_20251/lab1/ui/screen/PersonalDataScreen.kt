2e4642863791660627bef51b9091d8e16cbfd343
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2025-04-26 08:22:51 -0500

    feat: add the all general design of the PersonalDataScreen

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr08_20251/lab1/ui/screen/PersonalDataScreen.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr08_20251/lab1/ui/screen/PersonalDataScreen.kt
@@ -1,26 +1,47 @@
 package co.edu.udea.compumovil.gr08_20251.lab1.ui.screen
 
+import android.R.attr.type
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Label
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -29,11 +50,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -43,6 +69,9 @@ import co.edu.udea.compumovil.gr08_20251.lab1.ui.common.Header
 import co.edu.udea.compumovil.gr08_20251.lab1.ui.theme.balinookBold
 import co.edu.udea.compumovil.gr08_20251.lab1.ui.theme.balinookRegular
 import co.edu.udea.compumovil.gr08_20251.lab1.ui.viewModel.PersonalDataViewModel
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @Composable
 fun PersonalDataScreen(
@@ -88,8 +117,261 @@ fun Form(
         maxComponentWidth = 0.7f
     )
 
-    Button(onClick = { personalDataViewModel.checkUserInputs(navigateToContactData) }) {
-        Text(text = "Next")
+    var selectedGender by remember { mutableStateOf("Masculino") }
+
+    GenderSelection(
+        selectedOption = selectedGender,
+        onOptionSelected = { selectedGender = it }
+    )
+
+    DatePickerFieldToModal()
+
+    var selectedEducationLevel by remember { mutableStateOf("") }
+
+    EducationLevelDropdown(
+        selectedOption = selectedEducationLevel,
+        onOptionSelected = { selectedEducationLevel = it }
+    )
+
+    Row(
+        modifier = Modifier
+            .padding(top = 30.dp, end = 5.dp)
+            .fillMaxWidth(),
+    ) {
+        Button(
+            onClick = { personalDataViewModel.checkUserInputs(navigateToContactData) },
+            modifier = Modifier
+        ) {
+            Text(text = "Next")
+        }
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EducationLevelDropdown(
+    selectedOption: String,
+    onOptionSelected: (String) -> Unit
+) {
+    val options = listOf("Primaria", "Secundaria", "Universidad", "Otro")
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(start = 10.dp, bottom = 5.dp, top = 10.dp)
+            .fillMaxWidth(1f),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        CustomIcon(
+            iconId = R.drawable.school,
+            iconSize = 40.dp
+        )
+
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded }
+        ) {
+            TextField(
+                value = selectedOption,
+                onValueChange = {},
+                readOnly = true,
+                label = {
+                    Text(
+                        text = "Grado de escolaridad",
+                        fontFamily = balinookBold,
+                        color = Color.Gray
+                    )
+                },
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+                },
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = MaterialTheme.colorScheme.secondary,
+                    unfocusedIndicatorColor = MaterialTheme.colorScheme.secondary,
+                    errorIndicatorColor = MaterialTheme.colorScheme.secondary,
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                ),
+                modifier = Modifier
+                    .menuAnchor(type = MenuAnchorType.PrimaryNotEditable, enabled = true)
+                    .fillMaxWidth(0.7f)
+            )
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                options.forEach { option ->
+                    DropdownMenuItem(
+                        text = {
+                            Text(
+                                text = option,
+                                fontSize = 15.sp
+                            )
+                        },
+                        onClick = {
+                            onOptionSelected(option)
+                            expanded = false
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DatePickerFieldToModal(modifier: Modifier = Modifier) {
+    var selectedDate by remember { mutableStateOf<Long?>(null) }
+    var showModal by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(start = 10.dp, bottom = 5.dp, top = 10.dp)
+            .fillMaxWidth(1f),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        CustomIcon(
+            iconId = R.drawable.calendar_month,
+            iconSize = 40.dp
+        )
+
+        Text(
+            text = "Fecha de cumpleaños: ",
+            fontFamily = balinookBold,
+            fontSize = 15.sp
+        )
+
+        OutlinedTextField(
+            value = selectedDate?.let { convertMillisToDate(it) } ?: "",
+            onValueChange = { },
+            placeholder = {
+                Text(
+                    text = "MM/DD/YYYY",
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    fontSize = 14.sp
+                )
+            },
+            textStyle = LocalTextStyle.current.copy(
+                textAlign = TextAlign.Center,
+                fontSize = 14.sp
+            ),
+            modifier = modifier
+                .fillMaxWidth(0.7f)
+                .height(50.dp)
+                .pointerInput(selectedDate) {
+                    awaitEachGesture {
+                        awaitFirstDown(pass = PointerEventPass.Initial)
+                        val upEvent = waitForUpOrCancellation(pass = PointerEventPass.Initial)
+                        if (upEvent != null) {
+                            showModal = true
+                        }
+                    }
+                },
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = MaterialTheme.colorScheme.primary,
+                unfocusedIndicatorColor = MaterialTheme.colorScheme.primary,
+                errorIndicatorColor = MaterialTheme.colorScheme.primary,
+                focusedContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+            )
+        )
+    }
+
+
+    if (showModal) {
+        DatePickerModal(
+            onDateSelected = { selectedDate = it },
+            onDismiss = { showModal = false }
+        )
+    }
+}
+
+fun convertMillisToDate(millis: Long): String {
+    val formatter = SimpleDateFormat("MM/dd/yyyy", Locale.getDefault())
+    return formatter.format(Date(millis))
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerModal(
+    onDateSelected: (Long?) -> Unit,
+    onDismiss: () -> Unit
+) {
+    val datePickerState = rememberDatePickerState()
+
+    DatePickerDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(onClick = {
+                onDateSelected(datePickerState.selectedDateMillis)
+                onDismiss()
+            }) {
+                Text("OK")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Cancel")
+            }
+        }
+    ) {
+        DatePicker(state = datePickerState)
+    }
+}
+
+@Composable
+fun GenderSelection(
+    selectedOption: String,
+    onOptionSelected: (String) -> Unit
+) {
+    val options = listOf("Hombre", "Mujer")
+
+    Row(
+        modifier = Modifier
+            .padding(start = 10.dp, bottom = 5.dp, top = 15.dp)
+            .fillMaxWidth(1f),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(5.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(15.dp)
+        ) {
+            CustomIcon(
+                iconId = R.drawable.group,
+                iconSize = 40.dp
+            )
+            Text(
+                text = "Sexo:",
+                fontFamily = balinookBold,
+                fontSize = 17.sp
+            )
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(15.dp)
+        ) {
+            options.forEach { option ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(0.dp)
+                ) {
+                    RadioButton(
+                        selected = selectedOption == option,
+                        onClick = { onOptionSelected(option) },
+                    )
+                    Text(
+                        text = option,
+                        modifier = Modifier.offset(x = (-7).dp)
+                    )
+                }
+            }
+
+        }
     }
 }
 
